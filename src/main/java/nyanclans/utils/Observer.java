41eaf70a2ada9bc6ca996ff7e031d45f024c33c1
Vasliliy <vasyokbelyiv@gmail.com>
2019-03-14 04:48:30 +0500

    Add observer patter interfaces

---
+++ src/main/java/nyanclans/utils/Observer.java
@@ -0,0 +1,23 @@
+/*
+ * This file is part of NyanClans Bukkit plug-in.
+ *
+ * NyanClans is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NyanClans is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NyanClans. If not, see <https://www.gnu.org/licenses/>.
+ */
+package nyanclans.utils;
+
+/** @author nyanguymf */
+@FunctionalInterface
+public interface Observer<ObserveredType> {
+    void update(ObserveredType obs);
+}
