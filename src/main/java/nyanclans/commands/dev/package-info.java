7eaec8505a50bb187ab36955c7e90b7e0a43e252
Vasliliy <vasyokbelyiv@gmail.com>
2019-03-15 03:44:33 +0500

    Add packages information

---
+++ src/main/java/nyanclans/commands/dev/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of NyanClans Bukkit plug-in.
+ *
+ * NyanClans is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NyanClans is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NyanClans. If not, see <https://www.gnu.org/licenses/>.
+ */
+/**
+ * Provides main developer command executor
+ * {@link nyanclans.commands.dev.DeveloperCommand}
+ * and tab completer for this plug-in command
+ * {@link nyanclans.commands.dev.DeveloperCompleter}.
+ *
+ * @author NyanGuyMF
+ */
+package nyanclans.commands.dev;
