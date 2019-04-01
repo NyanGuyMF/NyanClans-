10c5788ccaf1f3207189693003d22933078b0613
Vasliliy <vasyokbelyiv@gmail.com>
2019-04-02 02:28:54 +0500

    Implement getter for last invite with Apache collections

--- src/main/java/nyanclans/storage/cache/InviteCache.java
+++ src/main/java/nyanclans/storage/cache/InviteCache.java
@@ -17,12 +17,13 @@
 package nyanclans.storage.cache;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+
+import org.apache.commons.collections4.map.LinkedMap;
 
 /** @author NyanGuyMF - Vasiliy Bely */
 public final class InviteCache {
-    private static volatile Map<String, LinkedHashMap<String, Invite>> cache;
+    private static volatile Map<String, LinkedMap<String, Invite>> cache;
 
     public InviteCache() {
         // because of we have a static cache for all instances
@@ -44,6 +45,28 @@ public final class InviteCache {
 
         return getCachedPlayer(invite.getPlayer().getName())
                 .put(invite.getClan().getName(), invite);
+    }
+
+    /**
+     * Gets latest cached invite of given player.
+     * <p>
+     * If player wasn't cached yet it will return <tt>null</tt>.
+     *
+     * @param   player  Invited player.
+     * @return Cached invite or <tt>null</tt>.
+     */
+    public synchronized Invite getLastCachedInvite(final String player) {
+        LinkedMap<String, Invite> invites = getCachedPlayer(player);
+
+        // #lastKey() method may throw exception, so we have to check size too
+        if ((invites == null) || (invites.size() == 0))
+            return null;
+
+        // I thought about the the fastest way to get last element,
+        // used Iterator with #hasNex() #next() methods, but I found, that
+        // Apache Commons LinkedMap already have last element method, so I use it
+
+        return invites.get(invites.lastKey());
     }
 
     /**
@@ -75,7 +98,7 @@ public final class InviteCache {
      * @return Map of invites from different clans of given player
      *      or <tt>null</tt> if doesn't exist.
      */
-    public synchronized LinkedHashMap<String, Invite> getCachedPlayer(final String player) {
+    public synchronized LinkedMap<String, Invite> getCachedPlayer(final String player) {
         return InviteCache.cache.get(player);
     }
 
@@ -85,7 +108,7 @@ public final class InviteCache {
      * @param   player  Player, whose invites you want to remove.
      * @return Map with invites of player.
      */
-    public synchronized LinkedHashMap<String, Invite> removeCachedPlayer(final String player) {
+    public synchronized LinkedMap<String, Invite> removeCachedPlayer(final String player) {
         return InviteCache.cache.remove(player);
     }
 
@@ -145,6 +168,6 @@ public final class InviteCache {
         if (isPlayerCached(player))
             return;
 
-        InviteCache.cache.put(player, new LinkedHashMap<String, Invite>());
+        InviteCache.cache.put(player, new LinkedMap<String, Invite>());
     }
 }
